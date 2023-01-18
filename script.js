0b810e9cf98af675281f342908d7747373db5c44
isaac-grandy <isaaclgrandy@gmail.com>
2023-01-18 09:16:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let fullName = "Isaac Grandy";
+console.log(fullName);
+
+let age = 19;
+let birthday = "March 18";
+const lifeEvents = ["OCC", "WBHS", "Violin", "Guitar"];
+
+console.log(
+  "My name is " +
+    fullName +
+    " and I'm not into pineapples on pizza. I am currently " +
+    age +
+    " years old and my birthday is on " +
+    birthday
+);
